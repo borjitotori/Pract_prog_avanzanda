4ad29a829920e7fac35f334aead1dee1abdb4f3b
borjitotori <33838047+borjitotori@users.noreply.github.com>
2018-11-29 16:36:40 +0100

    General update for minor problems
    -Solved problem on code
    -Implemeted translation sumi-definitive form

--- src/Practica_1/Main.java
+++ src/Practica_1/Main.java
@@ -35,32 +35,59 @@ public class Main {
 		Product pelicula1 = new Product(03,"Deadpool", 20.00, true, peliculas);
 		Scanner cap = new Scanner(System.in);
 		
-		//hago un switch con el cual se hara el menu
-		System.out.println("1.Introduce nombre de usuario y contraseña");
-		System.out.println("2.Mostrar productos");
-		System.out.println("3.Cambiar nombre de usuario");
-		System.out.println("4.Cambiar contraseña del usuario");
-		System.out.println("5.Salir del programa");
-		int choice = cap.nextInt();
-
-		// if there's no user detected it forces the new user on create a new one
+		int lenguage = 0;
+		Scanner len = new Scanner(System.in);
+		System.out.println("Elija idioma");
+		System.out.println("1. English");
+		System.out.println("2. Español");
+		System.out.println("3. Français");
+		lenguage = len.nextInt();
+		
+		if(lenguage == 1) {
+			
+			LenChoose.setCountry("US");
+			LenChoose.setLanguage("en");
+			
+		}
+		if(lenguage == 2) {
+			
+			LenChoose.setCountry("ES");
+			LenChoose.setLanguage("es");
+			
+		}
+		if(lenguage == 3) {
+			
+			LenChoose.setCountry("FR");
+			LenChoose.setLanguage("fr");
+			
+		}
+		
+		if(lenguage !=1 && lenguage != 2 && lenguage!=3) {
+			
+			System.out.println("Valor invalido.");
+			
+		}
+		
 		while (!userdetected) {
-			Scanner opt = new Scanner(System.in);
+			Scanner sc = new Scanner(System.in);
 			
 			System.out.println("Para entrar tendras que crear un usuario, una contraseña y un Id");
 			System.out.println("");
 			System.out.println("Introduce tu Id:");
-			id = opt.nextLine();
+			id = sc.nextLine();
+			Scanner sd = new Scanner(System.in);
 			System.out.println("Introduce un nombre de usuario:");
-			usuario = opt.nextLine();
+			usuario = sd.nextLine();
+			Scanner se = new Scanner(System.in);
 			
 			String proof = "";
 				while(!passdetected) {
 			
 					System.out.println("Introduce una contraseña: ");
-					contra = opt.nextLine();
+					contra = se.nextLine();
+					Scanner st = new Scanner(System.in);
 					System.out.println("Introduce la contraseña otra vez: ");
-					proof = opt.nextLine();
+					proof = st.nextLine();
 			
 						if(contra.equals(proof)) {
 							passdetected = true;
@@ -69,13 +96,28 @@ public class Main {
 							mainproxy.setMainId(id);
 							mainproxy.setMainPassword(contra);
 		}
+
+		//hago un switch con el cual se hara el menu
+		LenChoose.translate();
+						
+    	System.out.println(LenChoose.getSring5());
+		System.out.println("1."+ LenChoose.getSring8());
+		System.out.println("2."+ LenChoose.getSring5());
+		System.out.println("3."+ LenChoose.getSring6());
+		System.out.println("4."+ LenChoose.getSring7());
+		System.out.println("5."+ LenChoose.getSringexir());  
+	   	System.out.println(LenChoose.getSring4());
+		int choice = cap.nextInt();
+
+		
+
 		
 		String newUser, newPass, newId;				
 						
 		while (menu) {
 			switch (choice) {
-					
-			//test if user is registered
+			//introduce el usuario y la contraseña comprobando si esta registrado
+			
 			case 1:
 				Scanner log = new Scanner(System.in);
 				String usuarioconf = "",pass = "";
@@ -87,8 +129,7 @@ public class Main {
 					confirmpass = true;
 					confirmuser = true;
 				}
-					
-			//shows prod
+				//muestra de productos
 			case 2:
 				if (confirmpass == true || confirmuser == false) {
 					for(int i=0; i < Category.listproducts.size();i++) {
@@ -101,8 +142,7 @@ public class Main {
 				} else 
 					Gestion.errorLogin(confirmuser, confirmpass);
 				break;
-					
-			//it allows to change username from proxy	
+				
 			case 3:
 				if (confirmpass == true || confirmuser == false) {
 					Scanner username = new Scanner(System.in);
@@ -112,15 +152,13 @@ public class Main {
 					break;
 				}
 				break;
-			
-			//it allows to change password from proxy
+				
 			case 4:
-				if (confirmpass == true || confirmuser == false) {
-					Scanner passpass = new Scanner(System.in);
-					System.out.println("Introduce la nueva contraseña: ");
-					newPass = passpass.nextLine();
-					mainproxy.setMainPassword(newPass);
-				}
+
+				Scanner passpass = new Scanner(System.in);
+				System.out.println("Introduce la nueva contraseña: ");
+				newPass = passpass.nextLine();
+				mainproxy.setMainPassword(newPass);
 				break;
 				
 			case 5:
